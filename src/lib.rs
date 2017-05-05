df8c280d7e6f2f3acfa4c8f50974b63e727a7157
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2017-05-05 15:45:05 +0200

    More documentation

--- src/lib.rs
+++ src/lib.rs
@@ -125,6 +125,10 @@ impl core::iter::FromIterator<f64> for Average {
     }
 }
 
+/// Assert that two numbers are almost equal to each other.
+///
+/// On panic, this macro will print the values of the expressions with their
+/// debug representations.
 macro_rules! assert_almost_eq {
     ($a:expr, $b:expr, $prec:expr) => (
         if ($a - $b).abs() > $prec {
