0098668d3b60b33725e7b31b6f1f4fe0373e535f
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2017-06-27 12:40:09 +0200

    Add a reference for calculating moments

--- src/moments/mod.rs
+++ src/moments/mod.rs
@@ -9,5 +9,8 @@ include!("variance.rs");
 include!("skewness.rs");
 include!("kurtosis.rs");
 
+// It is possible to calculate higher moments the same way,
+// see https://doi.org/10.1007/s00180-015-0637-z.
+
 /// Alias for `Variance`.
 pub type MeanWithError = Variance;
