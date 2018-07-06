0faada0687c519dd9d3d87d96811e1f8b8d16dfe
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2018-07-06 10:56:35 +0200

    Fix benchmarks

--- benches/min.rs
+++ benches/min.rs
@@ -10,14 +10,15 @@ use bencher::Bencher;
 
 /// Create a random vector of random floats in [0, 1].
 fn initialize_vec() -> Vec<f64> {
-    use rand::distributions::{Range, IndependentSample};
+    use rand::distributions::{Range, Distribution};
     use rand::{XorShiftRng, SeedableRng};
     let range = Range::new(0.0, 1.0);
     let n = 1_000_000;
     let mut values = Vec::with_capacity(n);
-    let mut rng = XorShiftRng::from_seed([1, 2, 3, 4]);
+    let mut rng = XorShiftRng::from_seed(
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]);
     for _ in 0..n {
-        values.push(range.ind_sample(&mut rng));
+        values.push(range.sample(&mut rng));
     }
     values
 }
